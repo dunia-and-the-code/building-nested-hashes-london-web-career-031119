c8e024597b5a381f25e0dc237749e48b0d81d40e
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-08 10:38:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,4 +14,4 @@ def third_challenge
       :heroine_friends => [ { :name => "Steven", :age => "30", :attitude => "confused"},
                             { :name => "Nurse", :age => "44", :attitude => "worried" } ]
    }                          
-}
+end
